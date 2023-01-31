c4483f66b697f08a61a9d3d9c48a1c86c872753f
Ryan Gibb <ryan@freumh.org>
2023-01-31 12:10:41 +0000

    eio networking

--- src/dns.ml
+++ src/dns.ml
@@ -1,6 +1,9 @@
 
-let main ~stdout =
-  Eio.Flow.copy_string "Hello, world!\n" stdout
+let handle_client flow _addr stdout =
+  Eio.Flow.copy_string "Hello from server" flow;
+  Eio.Flow.copy_string "Hello from server" stdout
 
 let () = Eio_main.run @@ fun env ->
-  main ~stdout:(Eio.Stdenv.stdout env);;
+  let flow = Eio_mock.Flow.make "flow" in
+  let addr = `Tcp (Eio.Net.Ipaddr.V4.loopback, 37568) in
+  handle_client flow addr (Eio.Stdenv.stdout env);;
