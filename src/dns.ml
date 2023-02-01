8af8493bf3b0f9797026078c5fac813f0c88e1f1
Ryan Gibb <ryan@freumh.org>
2023-02-01 12:32:37 +0000

    Use `Eio.Net.recv`

--- src/dns.ml
+++ src/dns.ml
@@ -1,12 +1,14 @@
 
 let handle_client sock _stdout =
-  let b = Cstruct.create 100 in
-  let _sock2, _i = sock#recv b in
+  let b = Cstruct.create 512 in
+  let _sock2, _i = Eio.Net.recv sock b in
   Eio.traceln "Client: received %S" (Cstruct.to_string b)
 
-let () =
-  Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun sw ->
+let main ~net ~stdout =
   let bindaddr = `Udp (Eio.Net.Ipaddr.V4.loopback, 53) in
-  let sock = Eio.Net.datagram_socket ~sw (Eio.Stdenv.net env) bindaddr in
-  handle_client sock (Eio.Stdenv.stdout env);;
+  Eio.Switch.run @@ fun sw ->
+  let sock = Eio.Net.datagram_socket ~sw net bindaddr in
+  handle_client sock stdout;;
+
+let () = Eio_main.run @@ fun env ->
+  main ~net:(Eio.Stdenv.net env) ~stdout:(Eio.Stdenv.stdout env)
