bf9402d6ceb94f615f95cc4e0499e67ca7191c4b
Ryan Gibb <ryan@freumh.org>
2023-02-06 16:50:19 +0000

    added `dns-server`

--- src/dns_server_eio.ml
+++ src/dns_server_eio.ml
@@ -1,5 +1,5 @@
 
-let handle_client sock _stdout =
+let listen sock _server =
   let buf = Cstruct.create 512 in
   while true do
     let addr, size = Eio.Net.recv sock buf in
@@ -12,12 +12,21 @@ let handle_client sock _stdout =
       Eio.Net.send sock addr buf_trim
   done
 
-let main ~net ~stdout =
+let main ~net ~random =
   Eio.Switch.run @@ fun sw ->
   let get_sock addr = Eio.Net.datagram_socket ~sw net (`Udp (addr, 53)) in
+  (* TODO load from zonefile *)
+  (* let zones, trie, keys = Dns_zone.decode_zones_keys bindings in *)
+  let trie = Dns_trie.empty in
+  let rng ?_g length =
+    let buf = Cstruct.create length in
+    Eio.Flow.read_exact random buf;
+    buf
+  in
+  let server = ref @@ Dns_server.Primary.create ~rng trie in
   Eio.Fiber.both
-    (fun () -> handle_client (get_sock Eio.Net.Ipaddr.V6.loopback) stdout)
-    (fun () -> handle_client (get_sock Eio.Net.Ipaddr.V4.loopback) stdout)
+    (fun () -> listen (get_sock Eio.Net.Ipaddr.V6.loopback) server)
+    (fun () -> listen (get_sock Eio.Net.Ipaddr.V4.loopback) server)
 
 let () = Eio_main.run @@ fun env ->
-  main ~net:(Eio.Stdenv.net env) ~stdout:(Eio.Stdenv.stdout env)
+  main ~net:(Eio.Stdenv.net env) ~random:(Eio.Stdenv.secure_random env)
