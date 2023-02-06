13f6ea42fbfb81cb8b7eb2d41fe3a53e2ee1e393
Ryan Gibb <ryan@freumh.org>
2023-02-06 18:03:23 +0000

    respond to resolution

--- src/dns_server_eio.ml
+++ src/dns_server_eio.ml
@@ -1,18 +1,33 @@
 
-let listen sock _server =
+let listen ~clock ~mono_clock sock server =
   let buf = Cstruct.create 512 in
   while true do
     let addr, size = Eio.Net.recv sock buf in
+    let src, port = match addr with
+      | `Udp (ip, p) -> 
+        let src = (ip :> string) in
+        let src = Eio.Net.Ipaddr.fold
+          ~v4:(fun _v4 -> Ipaddr.V4 (Result.get_ok @@ Ipaddr.V4.of_octets src))
+          ~v6:(fun _v6 -> Ipaddr.V6 (Result.get_ok @@ Ipaddr.V6.of_octets src))
+          ip
+        in
+        src, p
+    in
     let buf_trim = Cstruct.sub buf 0 size in
     Eio.traceln "received:"; Cstruct.hexdump buf_trim;
-    match Dns.Packet.decode buf_trim with
-    | Error e -> Dns.Packet.pp_err Fmt.stdout e
-    | Ok query ->
-      Dns.Packet.pp Fmt.stdout query; Fmt.flush Fmt.stdout ();
-      Eio.Net.send sock addr buf_trim
+    let now = Ptime.of_float_s @@ Eio.Time.now clock in
+    match now with
+    | None -> ()
+    | Some now ->
+      let ts = Mtime.to_uint64_ns @@ Eio.Time.Mono.now mono_clock in
+      (* todo handle these *)
+      let _t, answers, _notify, _n, _key =
+        Dns_server.Primary.handle_buf !server now ts `Udp src port buf
+      in
+      List.iter (Eio.Net.send sock addr) answers
   done
 
-let main ~net ~random =
+let main ~net ~random ~clock ~mono_clock =
   Eio.Switch.run @@ fun sw ->
   let get_sock addr = Eio.Net.datagram_socket ~sw net (`Udp (addr, 53)) in
   (* TODO load from zonefile *)
@@ -25,8 +40,12 @@ let main ~net ~random =
   in
   let server = ref @@ Dns_server.Primary.create ~rng trie in
   Eio.Fiber.both
-    (fun () -> listen (get_sock Eio.Net.Ipaddr.V6.loopback) server)
-    (fun () -> listen (get_sock Eio.Net.Ipaddr.V4.loopback) server)
+    (fun () -> listen ~clock ~mono_clock (get_sock Eio.Net.Ipaddr.V6.loopback) server)
+    (fun () -> listen ~clock ~mono_clock (get_sock Eio.Net.Ipaddr.V4.loopback) server)
 
 let () = Eio_main.run @@ fun env ->
-  main ~net:(Eio.Stdenv.net env) ~random:(Eio.Stdenv.secure_random env)
+  main
+    ~net:(Eio.Stdenv.net env)
+    ~random:(Eio.Stdenv.secure_random env)
+    ~clock:(Eio.Stdenv.clock env)
+    ~mono_clock:(Eio.Stdenv.mono_clock env) 
