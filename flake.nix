02d7fc0eba7889da32efeca4a4bda79fd7fe9569
Ryan Gibb <ryan@freumh.org>
2023-02-08 12:29:52 +0000

    create development environment
    required making a separate opam query for lsp because lsp is not compatible with ocaml >= 5.0.0
    
    To use this, run e.g. ``nix develop --impure -c dune build `pwd` ``

--- flake.nix
+++ flake.nix
@@ -4,27 +4,13 @@
     flake-utils.url = "github:numtide/flake-utils";
     opam-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+  outputs = { self, nixpkgs, flake-utils, opam-nix }@inputs:
     let package = "dns-server-eio";
     in flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        devPackagesQuery = {
-          # You can add "development" packages here. They will get added to the devShell automatically.
-          # ocaml-lsp-server = "*";
-          # ocamlformat = "*";
-        };
-        query = devPackagesQuery // {
-          ## You can force versions of certain packages here, e.g:
-          ## - force the ocaml compiler to be taken from opam-repository:
-          # ocaml-base-compiler = "*";
-          ## - or force the compiler to be taken from nixpkgs and be a certain version:
-          # ocaml-system = "4.14.0";
-          ## - or force ocamlfind to be a certain version:
-          # ocamlfind = "1.9.2";
-        };
-        scope = on.buildOpamProject' { } ./. query;
+        scope = on.buildOpamProject' { } ./. {};
         overlay = final: prev: {
           "${package}" = prev.${package}.overrideAttrs (_: {
             # Prevent the ocaml dependencies from leaking into dependent environments
@@ -34,19 +20,23 @@
         scope' = scope.overrideScope' overlay;
         # The main package containing the executable
         main = scope'.${package};
-        # Packages from devPackagesQuery
-        devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
       in {
         legacyPackages = scope';
 
-        packages.default = main;
+        packages = scope' // { default = main; };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ main ];
-          buildInputs = devPackages ++ [
-            # You can add packages from nixpkgs here
-          ];
-        };
+        devShells.default =
+          let
+            devPackagesQuery = {
+              ocaml-lsp-server = "*";
+              ocamlformat = "*";
+            };
+            devScope = on.queryToScope { } devPackagesQuery;
+            devPackages = builtins.attrValues
+              (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) devScope);
+          in pkgs.mkShell {
+           inputsFrom = [ main ];
+           buildInputs = devPackages;
+         };
       });
 }
