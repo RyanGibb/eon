80e2dd3b5afaaaedff35dee399763314f96b5640
Ryan Gibb <ryan@freumh.org>
2023-02-14 14:28:21 +0000

    MATERIALIZE

--- flake.nix
+++ flake.nix
@@ -9,35 +9,45 @@
     in flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        on = opam-nix.lib.${system};
+        opam-nix-lib = opam-nix.lib.${system};
         devPackagesQuery = {
           ocaml-lsp-server = "*";
           ocamlformat = "*";
         };
-        scope = on.buildOpamProject' { } ./. devPackagesQuery;
-        overlay = final: prev: {
-          "${package}" = prev.${package}.overrideAttrs (_: {
-            # Prevent the ocaml dependencies from leaking into dependent environments
-            doNixSupport = false;
-          });
-        };
-        scope' = scope.overrideScope' overlay;
-        # The main package containing the executable
-        main = scope'.${package};
-      in {
-        legacyPackages = scope';
-
-        packages = scope' // { default = main; };
-        defaultPackage = main;
-
-        devShells.default =
+        resolved-scope =
           let
-            devPackages = builtins.attrValues
-              (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
-          in pkgs.mkShell {
-           inputsFrom = [ main ];
-           buildInputs = devPackages;
-         };
+            scope = opam-nix-lib.buildOpamProject' { } ./. devPackagesQuery;
+            overlay = final: prev: {
+              "${package}" = prev.${package}.overrideAttrs (_: {
+                # Prevent the ocaml dependencies from leaking into dependent environments
+                doNixSupport = false;
+              });
+            };
+          in scope.overrideScope' overlay;
+        materialized-scope = opam-nix-lib.materializedDefsToScope { sourceMap.${package} = ./.; } ./package-defs.json;
+      in rec {
+        packages = {
+          resolved = resolved-scope;
+          materialized = materialized-scope;
+          default = materialized-scope.${package};
+        };
+        defaultPackage = packages.default;
+
+        devShells = 
+          let
+            mkDevShell = scope:
+              let
+                devPackages = builtins.attrValues
+                  (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
+              in pkgs.mkShell {
+                inputsFrom = [ scope.${package} ];
+                buildInputs = devPackages;
+              };
+          in rec {
+            resolved = mkDevShell resolved-scope;
+            materialized = mkDevShell materialized-scope;
+            default = materialized;
+          };
       }) // {
     nixosModules.default = {
       imports = [ ./module.nix ];
