6585f4e253e38aa7ef9c2f544c2d6315a383276e
Ryan Gibb <ryan@freumh.org>
2023-02-20 14:58:24 +0000

    add meaningful logging

--- src/dns_server_eio.ml
+++ src/dns_server_eio.ml
@@ -1,5 +1,20 @@
 
-let listen ~clock ~mono_clock sock server =
+let log_packet log direction addr buf = match log with
+  | false -> ()
+  | true ->
+    (match direction with
+    | `Rx -> Format.fprintf Format.std_formatter "<-"
+    | `Tx -> Format.fprintf Format.std_formatter "->");
+    Format.print_space ();
+    Eio.Net.Sockaddr.pp Format.std_formatter addr;
+    Format.print_space ();
+    match Dns.Packet.decode buf with
+    | Error _ -> Format.fprintf Format.std_formatter "error";
+    | Ok packet -> Dns.Packet.pp Format.std_formatter packet;
+    Format.print_space (); Format.print_space ();
+    Format.print_flush ()
+
+let listen ~clock ~mono_clock ~log sock server =
   let buf = Cstruct.create 512 in
   while true do
     let addr, _size = Eio.Net.recv sock buf in
@@ -13,16 +28,17 @@ let listen ~clock ~mono_clock sock server =
         in
         src, p
     in
+    log_packet log `Rx addr buf;
     (* todo handle these *)
     let _t, answers, _notify, _n, _key =
       let now = Ptime.of_float_s @@ Eio.Time.now clock |> Option.get in
       let ts = Mtime.to_uint64_ns @@ Eio.Time.Mono.now mono_clock in
       Dns_server.Primary.handle_buf !server now ts `Udp src port buf
     in
-    List.iter (Eio.Net.send sock addr) answers
+    List.iter (fun b -> log_packet log `Tx addr b; Eio.Net.send sock addr b) answers
   done
 
-let main ~net ~random ~clock ~mono_clock zonefile =
+let main ~net ~random ~clock ~mono_clock ~zonefile ~log =
   Eio.Switch.run @@ fun sw ->
   let _zones, trie = Dns_zone.decode_zones [ ("freumh.org", zonefile) ] in
   let rng ?_g length =
@@ -42,9 +58,9 @@ let main ~net ~random ~clock ~mono_clock zonefile =
      [0] https://www.rfc-editor.org/rfc/rfc3493#section-3.7
      [1] https://labs.apnic.net/presentations/store/2015-10-04-dns-dual-stack.pdf *)
   let sock = Eio.Net.datagram_socket ~sw net (`Udp (Eio.Net.Ipaddr.V6.any, 53)) in
-  listen ~clock ~mono_clock sock server
+  listen ~clock ~mono_clock ~log sock server
 
-let () = Eio_main.run @@ fun env ->
+let run log = Eio_main.run @@ fun env ->
   let zonefile =
     let ( / ) = Eio.Path.( / ) in
     Eio.Path.load ((Eio.Stdenv.fs env) / Sys.argv.(1)) in
@@ -53,4 +69,7 @@ let () = Eio_main.run @@ fun env ->
     ~random:(Eio.Stdenv.secure_random env)
     ~clock:(Eio.Stdenv.clock env)
     ~mono_clock:(Eio.Stdenv.mono_clock env)
-    zonefile
+    ~zonefile
+    ~log
+
+let () = run true
