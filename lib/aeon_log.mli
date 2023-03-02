42c5c275cade19a13aed9b2321545565e3adc7a1
Ryan Gibb <ryan@freumh.org>
2023-03-02 18:55:06 +0000

    aeon_log mli file

---
+++ lib/aeon_log.mli
@@ -0,0 +1,5 @@
+
+(* TODO is there a way to deduplciate these type signatures? *)
+val log_level_0 : Format.formatter -> [< `Rx | `Tx ] -> [< Eio.Net.Sockaddr.t ] -> Cstruct.t -> unit
+val log_level_1 : Format.formatter -> [< `Rx | `Tx ] -> [< Eio.Net.Sockaddr.t ] -> Cstruct.t -> unit
+val log_level_2 : Format.formatter -> [< `Rx | `Tx ] -> [< Eio.Net.Sockaddr.t ] -> Cstruct.t -> unit
