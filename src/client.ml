cea1fc3acf631f42b7db4ecb37e7181a58e21787
Ryan Gibb <ryan@freumh.org>
2023-03-23 14:58:32 +0000

    got the server replying with a cname
    had to hack up `Dns_server` to do this

--- src/client.ml
+++ src/client.ml
@@ -13,23 +13,27 @@ let udp_listen log sock handle_dns =
 let create_query ~rng record_type authority =
   let
     question =
-      let message = "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohhellohellohellohellohellohellohellohellohellohellohellohellohellohellohello" in
+      let message = "hello" in
+      Eio.traceln "%s" message;
       let data = Base64.encode_exn message in
       assert (String.length data + String.length authority < 255);
-      let rec segment_string list string =
+      let rec segment_string string =
         let max_len = 63 in
         let len = String.length string in
         if len > max_len then
           let segment = String.sub string 0 max_len  in
           let string = String.sub string max_len (len - max_len) in
-          segment_string (segment :: list) string
+          let list = segment_string string in
+          segment :: list
         else
-          string :: list
+          [ string ]
       in
-      let data_name = Array.of_list @@ segment_string [] data in
+      (* Eio.traceln "%s" data; *)
+      let data_name = Array.of_list @@ segment_string data in
       let root = Domain_name.(of_string_exn authority |> host_exn |> to_array) in
       let name_array = Array.append root data_name in
       let hostname = Domain_name.of_array name_array in
+      Eio.traceln "%s" @@ Domain_name.to_string hostname;
       Dns.Packet.Question.create hostname record_type and
     header =
       let flags = Dns.Packet.Flags.singleton `Recursion_desired in
@@ -41,7 +45,7 @@ let create_query ~rng record_type authority =
 
 let run hostname nameserver = Eio_main.run @@ fun env ->
   let
-    record_type = Dns.Rr_map.A and
+    record_type = Dns.Rr_map.Cname and
     (* TODO query ns *)
     addr = `Udp (Ipaddr.of_string_exn nameserver |> Util.convert_ipaddr_to_eio, 53)
   in
@@ -64,11 +68,20 @@ let run hostname nameserver = Eio_main.run @@ fun env ->
             | [ _key, relevant_map ] ->
               (match Dns.Rr_map.find record_type relevant_map with
               | None -> ()
-              | Some (_ttl, answer) ->
-                (match Ipaddr.V4.Set.choose_opt answer with
-                  | None -> ()
-                  | Some ip -> Eio.traceln "%s" @@ Ipaddr.V4.to_string ip; exit 0
-                )
+              | Some (_ttl, cname) ->
+                Eio.traceln "%s" @@ Domain_name.to_string cname;
+                match Domain_name.find_label cname (fun s -> String.equal "rpc" s) with
+                | None ->
+                  exit 1
+                | Some i ->
+                  let data_name = Domain_name.drop_label_exn ~rev:true ~amount:(Domain_name.count_labels cname - i) cname in
+                  (* let root = Domain_name.drop_label_exn ~amount:(i) cname in
+                  Eio.traceln "%s" @@ Domain_name.to_string root; *)
+                  let data_array = Domain_name.to_array data_name in
+                  let data = String.concat "" (Array.to_list data_array) in
+                  let message = Base64.decode_exn data in
+                  Eio.traceln "%s" message;
+                  exit 0
               )
               | _ -> ()
             )
