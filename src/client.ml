45baaaed25061af74a082158cdda36a6a6ff1c0f
Ryan Gibb <ryan@freumh.org>
2023-03-23 11:56:24 +0000

    encode data in query question domain

--- src/client.ml
+++ src/client.ml
@@ -12,22 +12,37 @@ let udp_listen log sock handle_dns =
     handle_dns trimmedBuf
   done
 
-let create_query ~rng record_type hostname =
+let create_query ~rng record_type authority =
       (* | `Tcp -> Some (Edns.create ~extensions:[Edns.Tcp_keepalive (Some 1200)] ()) *)
   let
-    question = Dns.Packet.Question.create hostname record_type and
+    question =
+      let message = "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohhellohellohellohellohellohellohellohellohellohellohellohellohellohellohello" in
+      let data = Base64.encode_exn message in
+      assert (String.length data + String.length authority < 255);
+      Eio.traceln "%d" (String.length data + String.length authority);
+      let rec segment_string list string =
+        let max_len = 63 in
+        let len = String.length string in
+        if len > max_len then
+          let segment = String.sub string 0 max_len  in
+          let string = String.sub string max_len (len - max_len) in
+          segment_string (segment :: list) string
+        else
+          string :: list
+      in
+      let data_name = Array.of_list @@ segment_string [] data in
+      let root = Domain_name.(of_string_exn authority |> host_exn |> to_array) in
+      let name_array = Array.append root data_name in
+      let hostname = Domain_name.of_array name_array in
+      Dns.Packet.Question.create hostname record_type and
     header =
       let flags = Dns.Packet.Flags.singleton `Recursion_desired in
       (* let flags =
         if dnssec then Dns.Packet.Flags.add `Authentic_data flags else flags
       in *)
-      Randomconv.int16 rng, flags and
-    additional =
-      Dns.Name_rr_map.union
-      (Dns.Name_rr_map.singleton (Domain_name.of_string_exn "rpc.example.org") Dns.Rr_map.Txt (1l, Dns.Rr_map.Txt_set.singleton "hello"))
-      (Dns.Name_rr_map.singleton (Domain_name.of_string_exn "rpc.example.org") Dns.Rr_map.Null (1l, Dns.Rr_map.Null_set.singleton (Bytes.of_string "hello")))
+      Randomconv.int16 rng, flags
   in
-  let query = Dns.Packet.create ~additional header question `Query in
+  let query = Dns.Packet.create header question `Query in
   (* Log.debug (fun m -> m "sending %a" Dns.Packet.pp query); *)
   let cs, _ = Dns.Packet.encode `Udp query in
   cs
@@ -39,7 +54,6 @@ let create_query ~rng record_type hostname =
 let run hostname nameserver = Eio_main.run @@ fun env ->
   let
     record_type = Dns.Rr_map.A and
-    name = Domain_name.(host_exn (of_string_exn hostname)) and
     (* TODO query ns *)
     addr = `Udp (Ipaddr.of_string_exn nameserver |> Util.convert_ipaddr_to_eio, 53)
   in
@@ -56,23 +70,28 @@ let run hostname nameserver = Eio_main.run @@ fun env ->
       udp_listen log sock (fun buf ->
         (* TODO deobfuscate this *)
         match Dns.Packet.decode buf with
-        | Ok packet -> (match packet.data with
-          | `Answer (answer, _authority) -> (
-            match Domain_name.Map.find_opt (Domain_name.raw name) answer with
-            | None -> () (* need soa *)
-            | Some relevant_map ->
-              match Dns.Rr_map.find record_type relevant_map with
+        | Ok packet ->
+          (match packet.data with
+          | `Answer (answer, _authority) ->
+            (match Domain_name.Map.bindings answer with
+            | [ _key, relevant_map ] ->
+              (match Dns.Rr_map.find record_type relevant_map with
               | None -> () (* TODO process cnames *)
               | Some (_ttl, answer) ->
-                match Ipaddr.V4.Set.choose_opt answer with
+                (match Ipaddr.V4.Set.choose_opt answer with
                   | None -> ()
-                  | Some ip -> Eio.traceln "%s" @@ Ipaddr.V4.to_string ip; exit 0)
-                  | _ -> ())
+                  | Some ip -> Eio.traceln "%s" @@ Ipaddr.V4.to_string ip; exit 0
+                )
+              )
+              | _ -> ()
+            )
+          | _ -> ()
+          )
         | _ -> ()
       )
     )
     (fun () ->
-      let query = create_query ~rng record_type name in
+      let query = create_query ~rng record_type hostname in
       Eio.Net.send sock addr query;
     )
 
