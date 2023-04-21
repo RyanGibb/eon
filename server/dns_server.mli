e903e49907cf30776fab29fadf08f72b3d87640e
Ryan Gibb <ryan@freumh.org>
2023-04-21 11:50:05 +0100

    make packet_callback optional

--- server/dns_server.mli
+++ server/dns_server.mli
@@ -144,12 +144,12 @@ module Primary : sig
     [packet], returning new state, an answer, and potentially notify packets to
     secondary name servers. *)
 
-  val handle_buf : s -> Ptime.t -> int64 -> proto ->
-    Ipaddr.t -> int -> Cstruct.t -> packet_callback ->
+  val handle_buf : ?packet_callback:packet_callback -> s -> Ptime.t -> int64 -> proto ->
+    Ipaddr.t -> int -> Cstruct.t ->
     s * Cstruct.t list * (Ipaddr.t * Cstruct.t list) list *
     [ `Notify of Soa.t option | `Signed_notify of Soa.t option | `Keep ] option *
     [ `raw ] Domain_name.t option
-  (** [handle_buf s now ts proto src src_port buffer packet_callback] decodes the
+  (** [handle_buf ~packet_callback s now ts proto src src_port buffer] decodes the
      [buffer], processes the DNS frame using {!handle_packet}, and encodes the reply.
      The result is a new state, potentially a list of answers to the requestor,
      a list of notifications to send out, information whether a notify (or
@@ -193,9 +193,9 @@ module Secondary : sig
     s * Packet.t option * (Ipaddr.t * Cstruct.t) option
   (** [handle_packet s now ts ip proto key t] handles the incoming packet. *)
 
-  val handle_buf : s -> Ptime.t -> int64 -> proto -> Ipaddr.t -> Cstruct.t ->
-    packet_callback -> s * Cstruct.t option * (Ipaddr.t * Cstruct.t) option
-  (** [handle_buf s now ts proto src packet_callback buf] decodes [buf], processes with
+  val handle_buf : ?packet_callback:packet_callback  -> s -> Ptime.t -> int64 -> proto -> Ipaddr.t -> Cstruct.t ->
+s * Cstruct.t option * (Ipaddr.t * Cstruct.t) option
+  (** [handle_buf ~packet_callback s now ts proto src buf] decodes [buf], processes with
       {!handle_packet}, and encodes the results. *)
 
   val timer : s -> Ptime.t -> int64 ->
